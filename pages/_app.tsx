5a76238354a01341934b1cbda680a625333c0d3d
Ashraf Benabderrazak <122945028+AshrafBen10@users.noreply.github.com>
2025-06-20 12:01:40 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import "@/styles/app.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useEffect } from "react";
+import { Auth } from "aws-amplify";
 
 Amplify.configure(outputs);
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, signOut }: AppProps & { signOut?: () => void }) {
+  useEffect(() => {
+    // Check if user is authenticated
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+        // Redirect to Canvas
+        window.location.href = "https://canvas.instructure.com/";
+      })
+      .catch((err) => {
+        console.log("Not authenticated", err);
+      });
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default withAuthenticator(App);
