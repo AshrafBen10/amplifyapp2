9ade2d011bddd2acd8ddf101cae775665cf1f9ae
Ashraf Benabderrazak <122945028+AshrafBen10@users.noreply.github.com>
2025-06-20 12:43:02 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import { getCurrentUser } from 'aws-amplify/auth';
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 Amplify.configure(outputs);
 
 function App({ Component, pageProps, signOut }: AppProps & { signOut?: () => void }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    // Check if user is authenticated
     const checkAuth = async () => {
       try {
         await getCurrentUser();
@@ -19,12 +20,18 @@ function App({ Component, pageProps, signOut }: AppProps & { signOut?: () => voi
         window.location.href = "https://canvas.instructure.com/";
       } catch (err) {
         console.log("Not authenticated", err);
+        setIsLoading(false);
       }
     };
     
     checkAuth();
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>; // Or any loading indicator you prefer
+  }
+
+  // This will only render if the user is not authenticated
   return <Component {...pageProps} />;
 }
 
